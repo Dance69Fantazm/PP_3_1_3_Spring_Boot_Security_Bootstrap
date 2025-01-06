782cccfb0907e8109bb5e68985b045d05807aea8
Denis Akhmetov <jira.gremory@gmail.com>
2025-01-06 04:30:51 +0300

    обновленный класс DataInitializer

--- src/main/java/ru/kata/spring/boot_security/demo/configs/DataInitializer.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/DataInitializer.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.configs;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.dao.RoleRepository;
 import ru.kata.spring.boot_security.demo.dao.UserRepository;
@@ -17,36 +18,46 @@ import java.util.Set;
 @Component
 public class DataInitializer {
 
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
+    private final UserService userService;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
-    public DataInitializer(UserRepository userRepository, RoleRepository roleRepository) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
+    @Autowired
+    public DataInitializer(UserService userService, RoleService roleService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
     }
-
-    User admin = new User("admin", "admin",(long)  30, "admin@mail.ru", "$2a$12$Y03tCK62VmaaZLeWlU8cnelB.m/Y4LOMgnC24UpxsJzOv2UE/Uc0K");
-    User user = new User("user", "user",(long)  30, "user@mail.ru", "$2a$12$oQKP9KHR5an3eHvvK2sGcOEW5Z0zomeLv2mopUN5DqOCJ7u5R9qCa");
-
-    Role roleAdmin = new Role("ROLE_ADMIN");
-    Role roleUser = new Role("ROLE_USER");
-    Set<Role> setAdmin = new HashSet<>();
-    Set<Role> setUser = new HashSet<>();
-
-
 
     @PostConstruct
-    public void initializationTable() {
-        roleRepository.save(roleAdmin);
-        roleRepository.save(roleUser);
+    private void initializationTable() {
+        roleService.save(new Role("ROLE_ADMIN"));
+        roleService.save(new Role("ROLE_USER"));
 
-        setAdmin.add(roleAdmin);
-        setAdmin.add(roleUser);
-        admin.setRoles(setAdmin);
-        userRepository.save(admin);
+        Set<Role> roleAdmin = new HashSet<>();
+        Set<Role> roleUser = new HashSet<>();
+        roleAdmin.add(roleService.findByRoleName("ROLE_ADMIN"));
+        roleUser.add(roleService.findByRoleName("ROLE_USER"));
 
-        setUser.add(roleUser);
-        user.setRoles(setUser);
-        userRepository.save(user);
+        User admin = new User();
+        admin.setName("admin");
+        admin.setSurname("admin");
+        admin.setAge(18L);
+        admin.setEmail("admin@mail.ru");
+        admin.setPassword(passwordEncoder.encode("admin"));
+        admin.setRoles(roleAdmin);
+
+        User user = new User();
+        user.setName("user");
+        user.setSurname("user");
+        user.setAge(18L);
+        user.setEmail("user@mail.ru");
+        user.setPassword(passwordEncoder.encode("user"));
+        user.setRoles(roleUser);
+
+        userService.saveUser(admin);
+        userService.saveUser(user);
     }
 }
+
+
